5f7702bf04c4d2029c250c5541403d51e7f14c79
skibidiby <skibidiby@outlook.com>
2023-05-31 19:49:25 +0300

    initial

---
+++ src/routes/+page.svelte
@@ -0,0 +1,35 @@
+<div class="home">
+    <div class="container">
+            <div class="content">
+			<h1>Hello, I am Victor!</h1>
+			<p>I like to code and stuff.</p>
+            </div>
+		</div>
+</div>
+
+<style>
+	.home {
+		display: flex;
+		justify-content: center;
+        align-items: center;
+        height: 100dvh;
+        background-color: rgb(0, 209, 164);
+	}
+	.home .container {
+        display: flex;
+        align-items: center;
+        background-color: rgb(48, 85, 250);
+        color: rgb(0, 209, 164);
+        margin-top: auto;
+		width: 100%;
+        height: 90%;
+        border-radius: 10px 10px 0px 0px;
+        border-color: rgba(238, 236, 236, 0.877);
+        border-width: 2px 0px 0px 0px;
+        border-style: solid;
+	}
+    .container .content{
+        align-items: center;
+        margin-left: 20px;
+    }
+</style>
