8f3ad39776abcc385fcf8b3c4935a9fe67c2da67
skibidiby <skibidiby@outlook.com>
2023-06-05 18:25:14 +0300

    lottie ae diagram added

--- src/routes/about-us/+page.svelte
+++ src/routes/about-us/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
+	import { LottiePlayer } from '@lottiefiles/svelte-lottie-player';
 	let ready: boolean = false;
 	onMount(() => (ready = true));
 </script>
@@ -10,11 +11,13 @@
 			<div class="content">
 				<h1>A bit about me</h1>
 				<p>
-					I have no idea how you landed here, but it's nice to meet you!
-					I am a young person with aburning passion about technologies and programming.
-                    I have professional experience in Web development mainly in React.js but in my spare time I love to work on personal projects with a wide variety of different tools.
+					I have no idea how you landed here, but it's nice to meet you! I am a young person with
+					aburning passion about technologies and programming. I have professional experience in Web
+					development mainly in React.js but in my spare time I love to work on personal projects
+					with a wide variety of different tools.
 				</p>
 			</div>
+			<LottiePlayer src="diagram.json" autoplay={true} />
 		{/if}
 	</div>
 </div>
@@ -39,8 +42,20 @@
 		margin-left: 20px;
 		font-size: 50px;
 	}
-	.container .content p{
+	.container .content p {
 		font-size: 30px;
-        max-width: 1000px;
+		width: 600px;
+	}
+	@media only screen and (max-width: 1200px) {
+		.about {
+			height: 100%;
+			width: 100%;
+		}
+		.about .container {
+			flex-direction: column;
+		}
+		.container .content p{
+			width: 100%;
+		}
 	}
 </style>
